17f2bcfe6cd1a2f9ba71a1439c3cbe0e8c2fa05b
ff18qd <ff18qd@gmail.com>
2017-10-16 02:19:58 +0000

    add list_schools and menu methods

--- lib/topschool_cli_app/cli.rb
+++ lib/topschool_cli_app/cli.rb
@@ -1,7 +1,28 @@
 class TopschoolCliApp::CLI
 
   def call
-    puts "top 10 engineering schools"
+    list_schools
+    menu
+  end
+
+  def list_schools
+    puts "Top 10 engineering schools in US:"
+    puts "top 10 EE schools"
+    puts "top 10 civil engineering schools"
+  end
+
+  def menu
+    input = " "
+    while input != "exit"
+      puts "Enter the ranking of school that you would like to know more about or type exit:"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "Top 1st school..."
+      when "2"
+        puts "Top 2nd school..."
+      end
+    end
   end
 
 end
