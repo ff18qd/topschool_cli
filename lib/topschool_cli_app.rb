cdaaa52664dc371443fe9b3ce002f4f1bc2edfc5
ff18qd <ff18qd@gmail.com>
2017-10-19 02:28:26 +0000

    Wednesday change

--- lib/topschool_cli_app.rb
+++ lib/topschool_cli_app.rb
@@ -1,9 +1,9 @@
 require_relative "./topschool_cli_app/version"
 require_relative "./topschool_cli_app/cli"
 require_relative "./topschool_cli_app/topschools.rb"
-# require 'open-uri'
-# require 'nokogiri'
-# require 'pry'
+require "open-uri"
+require "nokogiri"
+require "pry"
 
 
 
--- lib/topschool_cli_app/topschools.rb
+++ lib/topschool_cli_app/topschools.rb
@@ -2,39 +2,28 @@ class  TopschoolCliApp::Topschools
   attr_accessor :size, :age, :status, :research, :focus, :url, :name
 
   def self.electrical
-    # puts "1. MIT"
-    # puts "2. Stanford University"
-    # puts "3. UC Berkley"
+    self.scrape_schools
+  end
 
-    top1 = TopschoolCliApp::Topschools.new
-    top1.name = "MIT"
-    top1.size = "<12000"
-    top1.age = ">100 years"
-    top1.status = "Private"
-    top1.research = "Very High"
-    top1.focus = "Comprehensive"
-    top1.url = "https://www.topuniversities.com/universities/massachusetts-institute-technology-mit"
-
-    top2 = TopschoolCliApp::Topschools.new
-    top2.name = "Stanford University"
-    top2.size = "<30000"
-    top2.age = ">100 years"
-    top2.status = "Private"
-    top2.research = "Very High"
-    top2.focus = "Comprehensive"
-    top2.url = "https://www.topuniversities.com/universities/stanford-university"
-
-    top3 = TopschoolCliApp::Topschools.new
-    top3.name = "UC Berkeley"
-    top3.size = "<30000"
-    top3.age = ">100 years"
-    top3.status = "Public"
-    top3.research = "Very High"
-    top3.focus = "Comprehensive"
-    top3.url = "https://www.topuniversities.com/universities/university-california-berkeley-ucb"
-
-    [top1, top2, top3]
-
+  def self.scrape_schools
+    schools = []
+    schools << self.scrape_ee
+    # top1 = TopschoolCliApp::Topschools.new
+    # top1.name = "MIT"
+    # top1.size = "<12000"
+    # top1.age = ">100 years"
+    # top1.status = "Private"
+    # top1.research = "Very High"
+    # top1.focus = "Comprehensive"
+    # top1.url = "https://www.topuniversities.com/universities/massachusetts-institute-technology-mit"
+    schools
+  end
+  #
+  def self.scrape_ee
+    html = open("https://www.collegefactual.com/majors/engineering/ee-electrical-engineering/rankings/top-ranked/")
+    # html = open("https://www.topuniversities.com/university-rankings-articles/university-subject-rankings/top-engineering-schools-us")
+    doc = Nokogiri::HTML(html)
+    binding.pry
   end
 
 end
